d5e56a6800847c85c95da2f9658b58d0233857dc
yjyoo6831 <yjyoo6831>
2024-04-23 18:02:12 +0900

    240423

--- ex02_240423/app.js
+++ ex02_240423/app.js
@@ -22,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.use('/books',require('./routes/books.js'));
+app.use('/local',require('./routes/local.js'))
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
