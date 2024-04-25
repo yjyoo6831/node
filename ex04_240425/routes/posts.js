112160b6084ef6bfef4b21a686db6981635403ea
yjyoo6831 <yjyoo6831>
2024-04-25 18:30:30 +0900

    240425 posts,writes

--- ex04_240425/routes/posts.js
+++ ex04_240425/routes/posts.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db=require('../db.js');
+const { route } = require('./index.js');
 
 /* 게시판 목록페이지 이동 */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,7 @@ router.get('/', function(req, res, next) {
 });
 //게시판 목록 데이터 불러오기
 router.get('/list.json',function(req,res){
-    const sql='select * from posts order by pid desc';
+    const sql='select *,date_format(pdate,\'%Y %M %d %T\') as fdate from posts order by pid desc';
     db.get().query(sql,function(err,rows){
         if(err){
             console.log("게시판 목록 : error : ---- ",err)
@@ -17,5 +18,32 @@ router.get('/list.json',function(req,res){
         }
     });
 });
+//글쓰기 페이지로 이동
+router.get('/insert',function(req,res){
+    res.render('index.ejs',{title:'글쓰기',pageName:'posts/insert.ejs'})
+});
+//글을 DB저장
+router.post('/insert',function(req,res){
+    const title=req.body.title;
+    const contents=req.body.contents;
+    const uid=req.body.uid;
+    console.log(title,contents,uid);
+    const sql="insert into posts(title,contents,writer) values(?,?,?)";
 
+    db.get().query(sql,[title,contents,uid],function(err,rows){
+
+    });
+    res.redirect('/posts');
+
+});
+//게시글 read 페이지 이동
+router.get('/read',function(req,res){
+    const pid=req.query.pid;
+    console.log(pid);
+    const sql="select *,date_format(pdate,\'%Y %M %d %T\') as fdate from posts where pid=?";
+    db.get().query(sql,[pid],function(err,rows){
+        console.log(rows[0]);
+        res.render('index.ejs',{title:'게시글정보',pageName:'posts/read.ejs',post:rows[0] });
+    });
+});
 module.exports = router;
